0831fd3bb6c2f18cf492912817d7c992cf860553
Finnur Kolbeinsson <finkol@gmail.com>
2016-02-19 14:22:02 +0100

    Questions

--- ResponseTime/GameScene.swift
+++ ResponseTime/GameScene.swift
@@ -13,7 +13,7 @@ class GameScene: SKScene {
     let sprite = SKShapeNode(circleOfRadius: 100)
     
     var gameStartTime = NSTimeInterval()
-    var gameTime = NSTimeInterval()
+    var gameTime = Double()
     
     var gameTimeLabel = SKLabelNode()
     var questionLabel = UILabel()
@@ -28,56 +28,39 @@ class GameScene: SKScene {
     
     
     //var randomSecondDelayArray = [Double]()
-    var timeVisibleArray = [CGFloat]()
+    var timeVisibleArray = [Double]()
+    var timeVisible = CGFloat()
     
     var numberOfGames = 0
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
-        
-        if (numberOfGames < 10) {
-            createGame()
-        }
+        createGame()
+        createQuestion()
+        displayGame()
         
     }
+   
+    func timerUpdate(timer:NSTimer){
+        self.gameTime = NSDate.timeIntervalSinceReferenceDate() - gameStartTime
+        gameTimeLabel.text = String(format: "%.2f",gameTime)
+        var userInfo = timer.userInfo as! Dictionary<String, Double>
+        if self.gameTime > 5.2 {
+            displayQuestion(Double(timeVisible))
+            
+        }
+    }
     
-    func createGame() {
+    func displayGame() {
+        print ("yoloGame")
+        questionView.hidden = true
         gameStartTime = NSDate.timeIntervalSinceReferenceDate()
         
-        NSTimer.scheduledTimerWithTimeInterval(0.02,
-            target: self,
-            selector: Selector("timerUpdate:"),
-            userInfo: nil,
-            repeats: true)
-        
-        gameTimeLabel.fontColor = SKColor.blackColor()
-        gameTimeLabel.fontSize = 30
-        gameTimeLabel.name = "results"
-        gameTimeLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame)-300)
-        self.addChild(gameTimeLabel)
-        gameTimeLabel.hidden = false
-        
-        
-        
-        
-        screenHeight = screenSize.height
-        screenWidth = screenSize.width
-        
-        
-        
-        self.sprite.lineWidth = 2.0
-        self.sprite.name = "colorBall"
-        
-        
-        self.sprite.fillColor = UIColor.redColor()
-        self.sprite.position = CGPointMake(frame.size.width/2, frame.size.height/2)
-        
-        let randomSecondDelay = randomBetweenNumbers(0.5, secondNum: 4.5)
-        let timeVisible = randomBetweenNumbers(0.0, secondNum: 0.1)
-        
-        timeVisibleArray.append(timeVisible)
-        print(randomSecondDelay)
+        let randomSecondDelay = randomBetweenNumbers(0.5, secondNum: 4.8)
+        timeVisible = randomBetweenNumbers(0.0, secondNum: 0.08)
+        print(timeVisible)
+        timeVisibleArray.append(Double(timeVisible))
         
         NSTimer.scheduledTimerWithTimeInterval(
             Double(randomSecondDelay),
@@ -86,39 +69,108 @@ class GameScene: SKScene {
             userInfo: ["timeVisible": Double(timeVisible)],
             repeats: false)
         
-        self.addChild(self.sprite)
+        NSTimer.scheduledTimerWithTimeInterval(0.02,
+            target: self,
+            selector: Selector("timerUpdate:"),
+            userInfo: ["timeVisible": Double(timeVisible)],
+            repeats: true)
         
-        questionView.frame = CGRectMake(screenWidth/2, screenHeight/2, 200, 150)
-        questionView.hidden = false
-        questionView.backgroundColor = UIColor.blueColor()
-        
-        questionLabel.text = "Did the circle change?"
-        questionLabel.hidden = false
-        
-        questionView.addSubview(questionLabel)
-
-        self.view?.addSubview(questionView)
-        
-        
-        print (self)
-
+        print(timeVisibleArray)
     }
-   
-    func timerUpdate(timer:NSTimer){
-        self.gameTime = NSDate.timeIntervalSinceReferenceDate() - gameStartTime
-        gameTimeLabel.text = String(format: "%.2f",gameTime)
+    
+    func displayQuestion(timeVisible: Double) {
+        questionView.hidden = false
     }
     
     func colorUpdate(timer: NSTimer){
         self.sprite.fillColor = UIColor.orangeColor()
-        var userInfo = timer.userInfo as! Dictionary<String, Double>
-        let timeVisible = 0.001
-        print(userInfo["timeVisible"])
-        delay(userInfo["timeVisible"]! as Double) {
-        //delay(timeVisible as Double) {
+        //var userInfo = timer.userInfo as! Dictionary<String, Double>
+        delay(Double(timeVisible)) {
            self.sprite.fillColor = UIColor.redColor()
         }
     }
+    
+    func createQuestion() {
+        let questionViewWidthTemp = screenWidth*0.8
+        let questionViewHeightTemp = screenHeight*0.8
+        questionView.frame = CGRectMake(screenWidth/2-questionViewWidthTemp/2, screenHeight/2-questionViewHeightTemp/2, questionViewWidthTemp, questionViewHeightTemp)
+        questionView.hidden = true
+        questionView.backgroundColor = UIColor.whiteColor()
+        
+        questionLabel.text = "Did the circle change?"
+        questionLabel.hidden = false
+        questionLabel.textColor = UIColor.blackColor()
+        questionLabel.textAlignment = NSTextAlignment.Center
+        questionLabel.font = questionLabel.font.fontWithSize(25)
+        
+        let questionViewSize = questionView.frame.size
+        let questionViewWidth = questionViewSize.width
+        let questionViewHeight = questionViewSize.height
+        questionLabel.frame = CGRectMake(0, 0, questionViewWidth, questionViewHeight*0.15 )
+        
+        questionView.addSubview(questionLabel)
+        
+        let yesBtn = UIButton()
+        //yesBtn.titleLabel?.font = yesBt
+        yesBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        //yesBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
+        
+        yesBtn.setTitle("Yes", forState: UIControlState.Normal)
+        yesBtn.addTarget(self, action: "displayGame", forControlEvents: UIControlEvents.TouchUpInside)
+        //yesBtn.setTranslatesAutoresizingMaskIntoConstraints(false)
+        yesBtn.backgroundColor = UIColor.greenColor()
+        yesBtn.frame = CGRectMake(questionViewWidth-questionViewWidth*0.95, questionViewHeight - questionViewHeight*0.70, questionViewWidth-questionViewWidth*0.6, questionViewHeight*0.25)
+        //yesBtn.frame = CGRectMake(15, -50, 300, 500)
+        yesBtn.hidden = false
+        
+        questionView.addSubview(yesBtn)
+        
+        let noBtn = UIButton()
+        noBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        noBtn.setTitle("No", forState: UIControlState.Normal)
+        noBtn.addTarget(self, action: "displayGame", forControlEvents: UIControlEvents.TouchUpInside)
+        noBtn.backgroundColor = UIColor.redColor()
+        noBtn.frame = CGRectMake(questionViewWidth-questionViewWidth*0.45, questionViewHeight - questionViewHeight*0.70, questionViewWidth-questionViewWidth*0.6, questionViewHeight*0.25)
+        noBtn.hidden = false
+        
+        questionView.addSubview(noBtn)
+        
+        self.view?.addSubview(questionView)
+    }
+    func noTest() {
+        print ("no")
+    }
+    
+    func yesTest() {
+        print ("yes")
+    }
+    
+    func createGame() {
+        gameStartTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        gameTimeLabel.fontColor = SKColor.whiteColor()
+        gameTimeLabel.fontSize = 30
+        gameTimeLabel.name = "results"
+        gameTimeLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame)-300)
+        self.addChild(gameTimeLabel)
+        gameTimeLabel.hidden = false
+        
+        self.backgroundColor = UIColor.blackColor()
+
+        screenHeight = screenSize.height
+        screenWidth = screenSize.width
+        
+        self.sprite.lineWidth = 2.0
+        self.sprite.name = "colorBall"
+        
+        
+        self.sprite.fillColor = UIColor.redColor()
+        self.sprite.position = CGPointMake(frame.size.width/2, frame.size.height/2)
+        
+        self.addChild(self.sprite)
+        
+    }
+
     
     func randomBetweenNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat{
         return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
